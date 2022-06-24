a308e2a85ea99b271698b708bbc7530aa2104515
Pricelesssarah-ui <106806237+Pricelesssarah-ui@users.noreply.github.com>
2022-06-24 09:51:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const input = document.querySelector('input');
-const btn = document.querySelector('btn');
-const 
+ var input = document.querySelector('input');
+var btn = document.querySelector('.btn');
+var notcompleted = document.querySelector('.notcompleted');
+
+btn.addEventListener('click', addlist);
+
+function addlist(){
+  var newList = document.createElement('li');
+  var text = docuement.createTextNode(input.value);
+  notcompleted.append(newList);
+  newList.append(text);
+  input.value = '';
+}
+
+
